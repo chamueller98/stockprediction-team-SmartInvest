f78850744f4badd43018d6cc89b978cc98c4c7e6
Isabelle <isabelle.weber@student.unisg.ch>
2019-09-02 22:39:54 +0200

    new definitions in library, improvement of website

--- app.py
+++ app.py
@@ -3,5 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Hello, World!"
+name = input("How may we call you? ")
+def writename (name):
+    result = ("We kindly welcome you " + name + " have a look at your personalized table.")
+    return result
+print (writename(name))
+
+
+
+
+#code unten nur damit automatische Wiederholung und wir nicht jedesmal seite killen müssen sondern nur 
+# refresh, funktioniert jedoch momentan leider nicht..
+
+if __name__ == "__main__":
+    app.run(debug=True)
